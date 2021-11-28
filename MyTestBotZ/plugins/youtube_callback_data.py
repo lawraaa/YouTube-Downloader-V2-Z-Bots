2b49ffc7811227624a4976cfb0aa6b9fec53e741
Mad To AzenZio <89691212+madtoazenzio@users.noreply.github.com>
2021-11-28 16:22:51 +0530

    Update youtube_callback_data.py

--- MyTestBotZ/plugins/youtube_callback_data.py
+++ MyTestBotZ/plugins/youtube_callback_data.py
@@ -107,7 +107,7 @@ async def catch_youtube_dldata(c, q):
             media=filename,
             thumb=thumb_image_path,
             caption=os.path.basename(filename),
-            title=os.path.basename(filename) + f"\n\n© @TG_Utubebot"
+            title=os.path.basename(filename) + f"\n\n© @Z_Bots"
         )
 
     if cb_data.startswith("video"):
@@ -119,7 +119,7 @@ async def catch_youtube_dldata(c, q):
             width=width,
             height=height,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + f"\n\n© @TG_Utubebot",
+            caption=os.path.basename(filename) + f"\n\n© @Z_Bots",
             supports_streaming=True
         )
 
@@ -128,7 +128,7 @@ async def catch_youtube_dldata(c, q):
         med = InputMediaDocument(
             media=filename,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + f"\n\n© @TG_Utubebot",
+            caption=os.path.basename(filename) + f"\n\n© @Z_Bots",
         )
 
     if cb_data.startswith("docvideo"):
@@ -137,7 +137,7 @@ async def catch_youtube_dldata(c, q):
         med = InputMediaDocument(
             media=filename,
             thumb=thumb_image_path,
-            caption=os.path.basename(filename) + f"\n\n© @TG_Utubebot",
+            caption=os.path.basename(filename) + f"\n\n© @Z_Bots",
         )
     if med:
         loop.create_task(send_file(c, q, med, filename))
