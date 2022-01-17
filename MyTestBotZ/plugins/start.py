11a27abe744ddd5a8ed31f3f11cf58a8b230851e
メ™Asʏɴ Eᴅɪᴛᴢ​ゞメ <89691212+madtoazenzio@users.noreply.github.com>
2022-01-17 12:27:38 +0530

    Update start.py

--- MyTestBotZ/plugins/start.py
+++ MyTestBotZ/plugins/start.py
@@ -5,14 +5,14 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("⛄Cʜᴀɴɴᴇʟ", url="https://t.me/Z_Bots"), InlineKeyboardButton("⛄Cʀᴇᴀᴛᴏʀ", url="https://github.com/madtoazenzio") ],
+        [InlineKeyboardButton("⛄Cʜᴀɴɴᴇʟ", url="https://t.me/XRoid_BotZ"), InlineKeyboardButton("⛄Cʀᴇᴀᴛᴏʀ", url="https://t.me/Space4AFML") ],
         [InlineKeyboardButton(
-            "⛄ Jᴏɪɴ ɴᴏᴡ ⛄", url="https://t.me/Z_Bots")]
+            "⛄ Jᴏɪɴ ɴᴏᴡ ⛄", url="https://t.me/XRoid_BotZ")]
     ])
     
     welcomed = f"""Hᴇʏ <b>{message.from_user.first_name}</b>\n\nA Sɪᴍᴘʟᴇ Yᴏᴜᴛᴜʙᴇ Dᴏᴡɴʟᴏᴀᴅᴇʀ Bᴏᴛ Tʜᴀᴛ Cᴀɴ:
   ➠ Dᴏᴡɴʟᴏᴀᴅ Yᴏᴜᴛᴜʙᴇ Vɪᴅᴇᴏs
-  ➠ Dᴏᴡɴʟᴏᴀᴅ Aᴜᴅɪᴏ Fʀᴏᴍ Yᴏᴜᴛᴜʙᴇ Vɪᴅᴇᴏs \n\n Mᴀᴅᴇ Wɪᴛʜ ♥️ Bʏ <a href="https://telegra.ph/file/e981d066dc5b3b2525c5c.jpg">ᴢ_ʙᴏᴛs</a>
-©️MᴀɪɴᴛᴀɪɴᴇD Bʏ: <a href="https://t.me/space4aysn">Asʏɴ Eᴅɪᴛᴢ🇮🇳</a>"""
+  ➠ Dᴏᴡɴʟᴏᴀᴅ Aᴜᴅɪᴏ Fʀᴏᴍ Yᴏᴜᴛᴜʙᴇ Vɪᴅᴇᴏs \n\n Mᴀᴅᴇ Wɪᴛʜ ♥️ Bʏ <a href="https://telegra.ph/file/e981d066dc5b3b2525c5c.jpg">XRoiD_ʙᴏᴛs</a>
+©️MᴀɪɴᴛᴀɪɴᴇD Bʏ: <a href="https://t.me/Space4AFML">メ™Aʟꜰʀᴇᴅ Mᴀʀsʜᴇʟゞメ🇮🇳</a>"""
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
